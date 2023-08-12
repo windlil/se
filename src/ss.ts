168d5559dc4758816a00ac7200632fe5de29e43e
windlil <spacesouls@163.com>
2023-08-12 14:20:08 +0800

    chore: add new function

--- src/ss.ts
+++ src/ss.ts
@@ -1,16 +1,37 @@
 #!/usr/bin/env node
-import logSymbols from 'log-symbols'
 import open from 'open'
 import process from 'process'
-import pc from 'picocolors'
+import fs from 'fs'
+import { dataPath, searchAddress } from './data'
+
+
+function handleOptions(options: string[]) {
+  const target = options.join(' ')
+  return target
+}
+
+
+async function handleSearch(target) {
+  let currentAddress
+  fs.readFile(dataPath, async (err, res) => {
+    const data = res.toString()
+    currentAddress = JSON.parse(data).current ?? 0
+    const search = `${searchAddress[currentAddress]}${target}`
+    await open(search)
+  })
+}
 
 const options:string[] = process.argv.slice(2)
 
 if (options.length > 0) {
-  console.log(logSymbols.warning, pc.red("ss don't have options"))
-  process.exit(0)
+  const target = handleOptions(options)
+  await handleSearch(target)
 }
 
 ;(async function () {
+  if (options.length > 0) {
+    return
+  }
   await open('https:')
 })()
+
